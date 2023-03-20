9c4d3bd1e99cc817ec62cb52c87fa13d0fa08af1
root <konsortech@gmail.com>
2023-03-20 17:57:49 +0000

    initial commit

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,62 @@
+import store from '@/store'
+import { isTestnet } from '../../libs/utils'
+
+function processMenu() {
+  const chainMenus = []
+  const blockchains = []
+  Object.keys(store.state.chains.config).forEach(chain => {
+    const menu = {
+      title: chain,
+      logo: store.state.chains.config[chain].logo,
+      route: { name: 'dashboard', params: { chain } },
+    }
+    blockchains.push(menu)
+  })
+
+  if (blockchains.length > 1) {
+    chainMenus.push({ header: 'ecosystem' })
+    chainMenus.push({
+      title: 'blockchains',
+      children: blockchains,
+      tag: `${blockchains.length}`,
+      icon: 'https://konsortech.xyz/wp-content/uploads/2022/11/cropped-konsortech_logo3-32x32-1.png',
+    })
+  }
+  chainMenus.push({ header: 'LINKS' })
+  if (isTestnet()) {
+    chainMenus.push({
+      title: 'Mainnet Explorer',
+      href: 'https://explorer.konsortech.xyz',
+      icon: 'ChromeIcon',
+    })
+  } else {
+    chainMenus.push({
+      title: 'Testnet Explorer',
+      href: 'https://testnet-explorer.konsortech.xyz/',
+      icon: 'LifeBuoyIcon',
+    })
+  }
+  chainMenus.push({
+    title: 'Twitter',
+    href: 'https://twitter.com/Konsor_Tech',
+    icon: 'TwitterIcon',
+  })
+  chainMenus.push({
+    title: 'Github',
+    href: 'https://github.com/konsortech',
+    icon: 'GithubIcon',
+  })
+  chainMenus.push({
+    title: 'Discord',
+    href: 'https://discordapp.com/users/1016752596633002104',
+    icon: 'EyeIcon',
+  })
+  chainMenus.push({
+    title: 'Telegram',
+    href: 'https://t.me/ilhamnurizha',
+    icon: 'SendIcon',
+  })
+  return chainMenus
+}
+
+export default processMenu()
